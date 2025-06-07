6935d663e989335af0778617b8f1e9efef5ec828
AyaCare <1164183923@qq.com>
2025-06-07 08:13:49 +0800

    Add files via upload

---
+++ categories.js
@@ -0,0 +1,6 @@
+// categories.js
+const categories = [
+           { id: "auto-cards", name: { zh: "汽车名片", ru: "Автомобильные визитки" }, commission: 0.12 },
+            { id: "watch-straps", name: { zh: "手表带", ru: "Ювелирные ремешки и браслеты для часов" }, commission: 0.15 }
+
+];
